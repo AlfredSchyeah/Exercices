9f8825109d72a5776d582084c2ea61fbfcb917b0
Alfred Shayahmetov <Alfredmerc@yandex.ru>
2026-08-05 17:23:34 +0300

    deleted all garbage

--- Array-in-reverse-order/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿int[] array = new int[5];
-for (int i = 0; i < array.Length; i++)
-{
-    array[i] = int.Parse(Console.ReadLine());
-}
-
-Array.Reverse(array);
-
-Console.WriteLine(string.Join(", ", array));
