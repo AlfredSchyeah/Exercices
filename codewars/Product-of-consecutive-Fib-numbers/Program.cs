ff1d8c6b6715fb9b2c8955a75c7c99513043263c
Alfred Shayahmetov <Alfredmerc@yandex.ru>
2026-08-18 14:03:33 +0300

    Fubonacci prod of consecutive

---
+++ codewars/Product-of-consecutive-Fib-numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿int prod = int.Parse(Console.ReadLine());
+
+int Fibonacci(int n)
+{
+    if (n == 0 || n == 1) return n;
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+(int, int, bool) FindPair(int prod)
+{
+    int i = 0;
+    for (; ; i++)
+    {
+        long p = (long)Fibonacci(i) * Fibonacci(i + 1);
+
+        if (prod == p)
+        {
+            return (Fibonacci(i), Fibonacci(i + 1), true);
+        }
+        else if (prod < p)
+        {
+            return (Fibonacci(i), Fibonacci(i + 1), false);
+        }
+    }
+}
+
+var result = FindPair(prod);
+Console.WriteLine(result);
