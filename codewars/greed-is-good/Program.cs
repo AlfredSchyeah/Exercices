706cc3e3936b51e01bdd6892135d49051d4bc0cf
Alfred Shayahmetov <Alfredmerc@yandex.ru>
2026-08-24 13:02:16 +0300

    greed is good

---
+++ codewars/greed-is-good/Program.cs
@@ -0,0 +1,32 @@
+﻿int[] dice = [2, 2, 2, 3, 1];
+
+int score = 0;
+
+int[] counts = new int[7];
+
+foreach (int num in dice)
+{
+    counts[num]++;
+}
+
+for (int i = 2; i <= 6; i++)
+{
+    if (counts[i] >= 3)
+    {
+        score = i * 100;
+    }
+}
+
+if (counts[1] >= 3)
+{
+    score = 1000;
+}
+
+
+int remainingOnes = counts[1] >= 3 ? counts[1] - 3 : counts[1];
+int remainingFives = counts[5] >= 3 ? counts[5] - 3 : counts[5];
+
+score += remainingOnes * 100;
+score += remainingFives * 50;
+
+Console.WriteLine(score);
