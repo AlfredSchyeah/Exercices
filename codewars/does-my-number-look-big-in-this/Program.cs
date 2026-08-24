07d3fe2a9f55c762e49f13cde7e9a8da3a2343aa
Alfred Shayahmetov <Alfredmerc@yandex.ru>
2026-08-24 13:45:01 +0300

    is Armstrong number

---
+++ codewars/does-my-number-look-big-in-this/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(IsArmstrong(153));
+        Console.WriteLine(IsArmstrong(123));
+        Console.WriteLine(IsArmstrong(1634));
+        Console.WriteLine(IsArmstrong(371));
+    }
+
+    static bool IsArmstrong(int value)
+    {
+        int length = value.ToString().Length;
+
+        int number = 0;
+
+        foreach (char CharDigit in value.ToString())
+        {
+            int digit = CharDigit - '0';
+            int PowDigit = (int)Math.Pow(digit, length);
+            number = number + PowDigit;
+        }
+        return number == value;
+    }
+
+}
