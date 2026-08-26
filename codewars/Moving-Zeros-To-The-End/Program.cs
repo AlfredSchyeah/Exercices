cf644f6ce9a1c80f5d7938ca15504aadd7bbac0d
Alfred Shayahmetov <Alfredmerc@yandex.ru>
2026-08-26 15:22:10 +0300

    moving zeroes to the end

---
+++ codewars/Moving-Zeros-To-The-End/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(string.Join(", ", MoveZeroes(new int[] { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1 })));
+    }
+
+    static int[] MoveZeroes(int[] arr)
+    {
+        int[] result = arr.Where(x => x != 0)
+                            .Concat(arr.Where(x => x == 0))
+                            .ToArray();
+        return result;
+    }
+}
